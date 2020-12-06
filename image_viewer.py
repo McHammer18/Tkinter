399972f5d3a6fcc76393cbd20e51b6de8188d909
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-12-06 10:41:56 -0600

    added status bar

--- image_viewer.py
+++ image_viewer.py
@@ -17,9 +17,10 @@ def forward(img_num):
     img_label = Label(image=img_list[img_num - 1])
     bf = Button(window, text="Next", command=lambda: forward(img_num + 1))
     bb = Button(window, text="Back", command=lambda: backward(img_num - 1))
-    if img_num == 5:
+    if img_num == len(img_list):
         bf.configure(state=DISABLED)
     img_label.grid(row=0, column=0, columnspan=3)
+    status.configure(text="Image {} of {}".format(img_num, len(img_list)))
     bb.grid(row=1, column=0)
     bf.grid(row=1, column=2)
 
@@ -36,6 +37,7 @@ def backward(img_num):
     if img_num == 1:
         bb.configure(state=DISABLED)
     img_label.grid(row=0, column=0, columnspan=3)
+    status.configure(text="Image {} of {}".format(img_num, len(img_list)))
     bb.grid(row=1, column=0)
     bf.grid(row=1, column=2)
 
@@ -48,20 +50,23 @@ img_2 = ImageTk.PhotoImage(Image.open("highlights/DSC_0005.jpg"))
 img_3 = ImageTk.PhotoImage(Image.open("highlights/DSC_0168.jpg"))
 img_4 = ImageTk.PhotoImage(Image.open("highlights/DSC_0234.jpg"))
 img_5 = ImageTk.PhotoImage(Image.open("highlights/DSC_0011.jpg"))
+img_6 = ImageTk.PhotoImage(Image.open("highlights/DSC_0021.jpg"))
 
 # list to hold the images to cycle through
-img_list = [img_1, img_2, img_3, img_4, img_5]
-
+img_list = [img_1, img_2, img_3, img_4, img_5, img_6]
+total_img = len(img_list)
+# status bar
+status = Label(window, text="Image {} of {}".format(1, total_img), bd=5, relief=SUNKEN, anchor=E)
 # label that holds the image
 img_label = Label(image=img_1)
 img_label.grid(row=0, column=0, columnspan=3)
 
-bb = Button(window, text="Back",command=lambda: backward, state=DISABLED)
+bb = Button(window, text="Back", command=lambda: backward, state=DISABLED)
 be = Button(window, text="Exit", command=window.quit)
-bf = Button(window, text="Next",command=lambda: forward(2))
+bf = Button(window, text="Next", command=lambda: forward(2))
 
 bb.grid(row=1, column=0)
 be.grid(row=1, column=1)
 bf.grid(row=1, column=2)
-
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 window.mainloop()
